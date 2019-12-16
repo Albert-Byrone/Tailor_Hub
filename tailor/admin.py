c2f8cd68caac4bb6777ee0af6337224ddd5d56d2
Albert Byrone <albertbyrone1677@gmail.com>
2019-12-16 11:47:12 +0300

    register the models to the admin dashboard

---
+++ tailor/admin.py
@@ -0,0 +1,8 @@
+
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(BillingAddress)
+admin.site.register(Coupon)
