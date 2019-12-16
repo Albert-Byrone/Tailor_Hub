8b96994f8c3c181fcf2b0e22eb88e61e3b03c361
Albert Byrone <albertbyrone1677@gmail.com>
2019-12-16 11:47:33 +0300

    customize the admin section

--- tailor/admin.py
+++ tailor/admin.py
@@ -1,3 +1,38 @@
+from django.contrib import admin
+from .models import Item,OrderItem,Order,Profile,Comment,BillingAddress,Coupon
+
+
+def accept_refund(ModelAdmin,request,queryset):
+    queryset.update(refund_requested=False,refund_approved=True)
+accept_refund.short_description = 'Approve refund'
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'is_ordered',
+        'being_delivered',
+        'received',
+        'refund_requested',
+        'refund_approved',
+        'billing_address',
+        'payment',
+        'coupon'
+        ]
+    list_display_link = [
+        'user',
+        'billing_address',
+        'payment',
+        'coupon'
+    ]
+    list_filter = [
+        'is_ordered',
+        'being_delivered',
+        'received',
+        'refund_requested',
+        'refund_approved'
+    ]
+    actions =[accept_refund]
+
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
